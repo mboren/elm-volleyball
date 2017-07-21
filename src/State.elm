1a5df57e44e125bb867f73e629231517ad5be6d8
Martin Boren <martyboren@gmail.com>
2017-07-20 17:35:59 -0700

    Add timer
    This fires a Tick event roughly every 100ms.
    The ticks don't do anything yet, but this will be used for
    physics and the round timer.

--- src/State.elm
+++ src/State.elm
@@ -1,5 +1,8 @@
 module State exposing (init, update, subscriptions)
 
+import Time exposing (Time)
+import Task
+
 import Types exposing (..)
 
 init : (Model, Cmd Msg)
@@ -7,12 +10,36 @@ init =
   let
     p1 = Player (1000/4,600/3) (0,0)
   in
-    (Model True 0 0 1000 600 10 250 p1, Cmd.none)
+    (Model True 0 0 1000 600 10 250 p1, Task.perform Resume Time.now)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  (model, Cmd.none)
+  case msg of
+    Resume startTime ->
+      ({ model
+        | paused = False
+        , time = startTime
+        , delta = 0
+       }
+      , Cmd.none)
+    Tick newTime ->
+      let
+        dt = newTime - model.time
+      in
+        ( { model
+            | time = newTime
+            , delta = dt
+          }
+        , Cmd.none
+        )
+
+    _ ->
+      (model, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  case model.paused of
+    True ->
+      Sub.none
+    False ->
+      Time.every (100 * Time.millisecond) Tick
